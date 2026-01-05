fb9da9579a85b734f5dd039f7716b56062b9b363
Aditya-Ranjan1234 <aditya87331@gmail.com>
2026-01-05 23:38:12 +0530

    Fixes

--- clientside/app/oauth/consent/page.jsx
+++ clientside/app/oauth/consent/page.jsx
@@ -1,15 +1,20 @@
-import {createClient} from '../../../lib/supabase/clients';
+'use client';
 
+import { useEffect } from 'react';
+import { createClient } from '../../../lib/supabase/clients';
 
-export default function check(){
-  const supabase = createClient();
-  supabase.auth.signInWithOAuth({
-  provider: 'google',
-  options: {
-    redirectTo: window.location.origin
-  }
-})
-  return(
+export default function Check() {
+  useEffect(() => {
+    const supabase = createClient();
+    supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: window.location.origin
+      }
+    });
+  }, []);
+
+  return (
     <h1>This is pretty gay!</h1>
   );
 }
